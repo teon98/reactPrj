5fbd84b5ef47d902cb33ed6bead9e34e96fb8227
Chanran33 <k55416325@gmail.com>
2021-02-17 20:45:38 +0900

    practice react

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import LifeCycleSmaple from './LifeCycleSmaple';
+import ErrorBoundary from './ErrorBoundary';
+
+//랜덤 색상을 생성합니다.
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref)=>this.scrollBox=ref}/>
-        <button onClick={()=>this.scrollBox.scrollToBottom()}>
-          맨 밑으로 
-        </button>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSmaple color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Average from './Average';
+
+const App = () => {
+  return (
+    <div>
+      <Average />
+    </div>
+  );
+};
+
+export default App;
