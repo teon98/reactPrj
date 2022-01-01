c4c6a5bcf20d21ac2ee354f7d905bc7859ec1ad0
Tae Young <k55416325@gamil.com>
2022-01-02 02:57:03 +0900

    외부 API를 연동하여 뉴스 뷰어 만들기2

--- news-viewer/src/components/Categories.js
+++ news-viewer/src/components/Categories.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const categories = [
   {
@@ -41,31 +42,60 @@ const CategoriesBlock = styled.div`
     width: 100%;
     overflow-x: auto;
   }
+  justify-content: flex-start;
 `;
 
-const Category = styled.div`
+const Category = styled(NavLink)`
   font-size: 1.125rem;
   cursor: pointer;
   white-space: pre;
   text-decoration: none;
   color: inherit;
-  padding-bottom: 0.23rem;
-  padding-right: 20px;
+  padding-bottom: 0.25rem;
 
   &:hover {
     color: #495057;
   }
+
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+    &:hover {
+      color: #3bc9db;
+    }
+  }
+
+  margin-right: 30px;
+
+  ${(props) =>
+    props.active &&
+    css`
+      font-weight: 600;
+      border-bottom: 2px solid #22b8cf;
+      color: #22b8cf;
+      &:hover {
+        color: #3bc9db;
+      }
+    `}
 
   & + & {
     margih-left: 1rem;
   }
 `;
 
-const Categories = () => {
+const Categories = ({ onSelect, category }) => {
   return (
     <CategoriesBlock>
       {categories.map((c) => (
-        <Category key={c.name}>{c.text}</Category>
+        <Category
+          key={c.name}
+          activeClassName="active"
+          exact={c.name === 'all'}
+          to={c.name === 'all' ? '/' : `/${c.name}`}
+        >
+          {c.text}
+        </Category>
       ))}
     </CategoriesBlock>
   );
--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -16,44 +17,36 @@ const NewsListBlock = styled.div`
     }
 `;
 
-const sampleArticle = {
-  title: '제목',
-  description: '내용',
-  url: 'https://google.com',
-  urlToImage: 'https://via.placeholder.com/160',
-};
+// const sampleArticle = {
+//   title: '제목',
+//   description: '내용',
+//   url: 'https://google.com',
+//   urlToImage: 'https://via.placeholder.com/160',
+// };
 
-const NewsList = () => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    //async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=424308f1a0ba4ea590776db1df51d82b',
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
+const NewsList = ({ category }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=424308f1a0ba4ea590776db1df51d82b`,
+    );
+  }, [category]);
 
   //대기 중일 대
   if (loading) {
     return <NewsListBlock>대기 중 ...</NewsListBlock>;
   }
   //아직 articles 값이 설정되지 않았을 대
-  if (!articles) {
+  if (!response) {
     return null;
   }
+  //에러가발생했을 대
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
 
-  //articles 값이 유효할 때
+  //response 값이 유효할 때
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
